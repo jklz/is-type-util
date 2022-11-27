960c5826e6ae03b71e258c3c33ef37df84abb473
Jared Spencer <jklz@me.com>
2022-11-27 00:04:06 -0700

    Create jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+import type {Config} from '@jest/types';
+
+const jestConfig: Config.InitialOptions = {
+  rootDir: "./",
+};
+export default jestConfig;
