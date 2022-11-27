f80a570ad6eb732484a9907cd4571c8888f2ae80
Jared Spencer <jklz@me.com>
2022-11-27 00:07:14 -0700

    Create example-values.ts

---
+++ src/examples/example-values.ts
@@ -0,0 +1,24 @@
+
+export const exampleString: string = `string`;
+export const exampleStringOfNumber: string = `18`;
+export const exampleStringWithSpaces: string = ` a string `;
+export const exampleStringOfSpacesValue: string = `  `;
+export const exampleStringEmpty: string = ``;
+
+export const exampleNumber: number = 3.1234;
+export const exampleNumberNegative: number = -3.1234;
+export const exampleNumberNan: number = NaN;
+export const exampleNumberInt: number = 10;
+export const exampleNumberIntNegative: number = -10;
+
+
+export const exampleUndefined: undefined = undefined;
+
+export const exampleNull: null = null;
+
+export const exampleBool: boolean = true;
+export const exampleTrue: true = true;
+export const exampleBoolTrue: boolean = true;
+export const exampleFalse: false = false;
+export const exampleBoolFalse: boolean = false;
+
