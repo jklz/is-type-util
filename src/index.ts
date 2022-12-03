755e2f36a4317effec13f7440fd974775176d784
Jared Spencer <jklz@me.com>
2022-12-02 18:49:09 -0700

    adjusted exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-export {isBoolean} from './is-boolean'
-export {isDefined} from './is-defined'
-export {isNumber} from './is-number'
-export {isString} from './is-string'
-export {isUndefined} from './is-undefined'
+
+import {isArray} from './is-array';
+import {isBoolean} from './is-boolean';
+import {isDefined} from './is-defined';
+import {isNumber} from './is-number';
+import {isString} from './is-string';
+import {isUndefined} from './is-undefined';
+
+
+export {
+  isArray,
+  isBoolean,
+  isDefined,
+  isNumber,
+  isString,
+  isUndefined,
+};
+const isType = {
+  array: isArray,
+  boolean: isBoolean,
+  defined: isDefined,
+  number: isNumber,
+  string: isString,
+  undefined: isUndefined,
+};
+
+export default isType;
